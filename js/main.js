4c006a3691e24f059f9a0cfe6e414e134cb5e527
igor1980god <71593197+igor1980god@users.noreply.github.com>
2022-04-14 16:08:19 +0300

    Бетховен1+

---
+++ js/main.js
@@ -0,0 +1,74 @@
+
+
+
+
+
+
+
+
+
+$(function(){
+  
+   $('.slider__items').slick({
+     arrows: false,
+    autoplay:true,
+     slidesToShow:3,
+      responsive: [
+      {
+        breakpoint: 630,
+        settings: {
+          arrows: false,
+          autoplay:true,
+           slidesToShow:1,
+          
+          
+       
+        }
+      },
+    ]
+   });
+
+
+    
+
+
+	$(".header").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+
+ 
+
+  
+});
+
+
+$(document).ready(function() {
+
+	//E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+});
---
+++ main.js
@@ -0,0 +1,58 @@
+$(function(){
+  
+   $('.slider__items').slick({
+     arrows: false,
+    autoplay:true,
+     slidesToShow:3,
+      responsive: [
+      {
+        breakpoint: 630,
+        settings: {
+          arrows: false,
+          autoplay:true,
+           slidesToShow:1,
+          
+          
+       
+        }
+      },
+    ]
+   });
+
+
+    	//E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Спасибо!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+
+	$(".header").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+
+ 
+
+  
+});
